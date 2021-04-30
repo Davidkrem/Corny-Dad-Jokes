b9d4424a4a4d25369901636711edbdae86702fcf
David Kremenitzer <dkrem21@gmail.com>
2021-04-30 12:52:02 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# **Corny DAD Joke Generator**
+
+### **Introduction**
+
+- [**Corny DAD Joke Generator**](#corny-dad-joke-generator)
+    - [**Introduction**](#introduction)
+    - [**Technologies**](#technologies)
+    - [**Sources**](#sources)
+    - [**Other information**](#other-information)
+
+**This project is a fun Dad joke generator. Click to get endless Dad jokes.
+
+### **Technologies**
+
+- Javascript
+- HTML
+- CSS
+  
+### **Sources**
+
+Dad jokes from API: <https://icanhazdadjoke.com/>
+
+### **Other information**
---
+++ jokes.js
@@ -0,0 +1,56 @@
+const jokeButton = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.joke p');
+const loader = document.querySelector('.loader');
+
+function wait(ms = 0) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const buttonText = [
+        'Gross Dad!.',
+        '🤦🏻‍♂️',
+        'omg dad.',
+        'you are the worst',
+        'seriously',
+        'stop it.',
+        'please stop',
+        'that was like literally like the WORST one',
+        'Go play shuffleboard, DAD!',
+        'Go eat your early bird dinner, DAD!',
+];
+
+async function fetchJoke() {
+        // turn loader on
+        loader.classList.remove('hidden');
+        jokeButton.classList.add('hidden');
+        await wait(400);
+        const response = await fetch('https://icanhazdadjoke.com', {
+                headers: {
+                        Accept: 'application/json',
+                },
+        });
+        const data = await response.json();
+        // turn loader off
+        loader.classList.add('hidden');
+        jokeButton.classList.remove('hidden');
+        return data;
+}
+
+function randomItemFromArray(arr, not) {
+        const item = arr[Math.floor(Math.random() * arr.length)];
+        // recursion
+        if (item === not) {
+                console.log('AHH We already used that last time, look again!');
+                return randomItemFromArray(arr, not);
+        }
+        return item;
+}
+
+async function handleClick() {
+        const { joke } = await fetchJoke();
+        console.log(joke);
+        jokeHolder.textContent = joke;
+        jokeButton.textContent = randomItemFromArray(buttonText, jokeButton.textContent);
+}
+
+jokeButton.addEventListener('click', handleClick);
